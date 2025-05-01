c4f9691659b48512c17dfc2dbb2a92b7e3dc9472
rocb-ento <bs21rocb@leeds.ac.uk>
2025-05-01 12:27:05 +0100

    added additional sliders

--- nowcast_v0.R
+++ nowcast_v0.R
@@ -7,71 +7,35 @@ library(rnaturalearthdata)
 library(rnaturalearth)
 library(tmap)
 
-# gridid_to_coords <- function(ID, n_cols = 12) {
-#   row <- ceiling(ID / n_cols)
-#   col <- ID - (row - 1) * n_cols
-#   return(list(row = row, col = col))
-# }
-# complete_grid <- data.frame(ID = as.numeric(1:144))  # 12x12 = 144 cells
-# complete_grid <- complete_grid %>%
-#   mutate(
-#     coords = lapply(ID, gridid_to_coords),
-#     row = sapply(coords, function(x) x$row),
-#     col = sapply(coords, function(x) x$col)
-#   ) %>%
-#   select(-coords)
-# radar <- left_join(complete_grid, radar, by = "ID")
-# available_dates <- sort(unique(radar$Date[!is.na(radar$Date)]))
-# mostobs <- radar %>% 
-#   group_by(ID, Radar, Date) %>% 
-#   unique() %>%
-#   ungroup() %>%
-#   group_by(Date) %>% 
-#   summarise(count = n()) %>% 
-#   arrange()
-
 ### testdate
-date = as.Date("22/07/2021", format = "%d/%m/%Y")
+#date = as.Date("22/07/2021", format = "%d/%m/%Y")
 ###
+# =============================== #
+#  load data + maps 
+# =============================== #
+cvp_covariates <- st_read("C:/Users/rocon/OneDrive/Documents/insect-radar-AES/data/covariate_data/cvp_covariates.geojson") %>%
+  as.data.frame() %>% 
+  select(col_lat, col_long)
+
+cvp_locations <- cvp_covariates %>%
+  st_as_sf(., coords = c("col_long", "col_lat"), crs = 4326) 
 
 radar <- fread("C:/Users/rocon/OneDrive/Documents/insect-radar-AES/data/radar_abundance_110325.csv") %>%
   filter(!is.na(Latitude)) %>%
-  filter(Height == "dBZ_500") %>%
   mutate(Date = as.Date(Date)) %>%
   mutate(ID = as.numeric(gsub("GridID_", "", ID))) %>%
   st_as_sf(., coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE) 
-
-radar_f <- radar %>% 
-  filter(Date == date)
-
+# radar_f <- radar %>% 
+#   filter(Date == date)
 UK <- ne_countries(country = "united kingdom", type = "countries", scale = "medium")
 UK <- UK$geometry
 
-
-
-# Create the map with tmap
-tm_shape(UK) +
-  tm_fill(col = "lightgrey") +
-  tm_borders() +
-  tm_shape(radar_f) +
-  tm_dots(col = "N", palette = "viridis", style = "cont", legend.show = F) +
-  tm_layout(frame = FALSE)
-
-# ## leaflet version
+# =============================== #
 # 
-# library(leaflet)
-# 
-# leaflet(data = radar_f, options = leafletOptions(zoomControl = T, dragging = T)) %>%  #width = range(radar_f$Latitude), height = range(radar_f$Longitude))) %>%
-#   addPolygons(data = UK, fillColor = "grey", fillOpacity = 1, color = "white", weight = 1) %>%
-#   addCircleMarkers(
-#     ~Longitude, ~Latitude,
-#     color = ~colorNumeric("viridis", N)(N),  # Continuous color based on N
-#     radius = 4,
-#     popup = ~paste0("ID: ", ID, "<br>N: ", N)  # Popup displaying ID and N
-#   ) # %>%
-#   #setView(lng = mean(radar_f$Longitude), lat = mean(radar_f$Latitude), zoom = 5)
-
-
+# =============================== #
+available_dates <- sort(unique(format(radar$Date, format = "%m-%d")))
+available_dates <- as.numeric(gsub("-", ".", available_dates))
+library(shinyWidgets)
 ui <- shinyUI(fluidPage(
   
   # Application title
@@ -80,47 +44,61 @@ ui <- shinyUI(fluidPage(
   # Sidebar with a slider input 
   sidebarLayout(
     sidebarPanel(
-      selectInput("DatesMerge",
-                  "Select Date:",
-                  choices = available_dates,
-                  selected = available_dates[length(available_dates) %/% 2]), 
-      
-      
+      sliderInput("height", label = "Height",
+                  min = 100, max = 1700, step = 200, value = 500),
+      sliderInput("year", label = "Year",
+                  min = 2012, max = 2022, step = 1, value = 2016),
+      sliderTextInput("date", label = "Date",
+                      choices = available_dates, 
+                      selected = available_dates[1], 
+                      grid = TRUE) # Optional grid for visual guidance
     ),
+    
+      
+     # sliderInput("date", label = "Date", min = min(available_dates), max = max(available_dates), value = available_dates[100])),
+    
     
     mainPanel(
       # Increase the size of the plot by specifying height and width
-      plotOutput("PlotRadar", height = "700px", width = "1000px"))
-    
+      plotOutput("PlotRadar", height = "1000px", width = "500px"))
+   )
   )
-))
+)
 
 
-
+# year = 2022
+# doy = 4.12
+# date = as.Date(paste0(year, ".", doy), format = "%Y.%m.%d")
+# height = "500"
 server <- shinyServer(function(input, output) {
   
+  # cache the base map
+  base_map <- tm_shape(UK) +
+    tm_fill(col = "darkgrey") +
+    tm_borders() +
+   # tm_layout(frame = FALSE) +
+    tm_shape(cvp_locations) +
+    tm_dots(col = "lightgrey")
+  
   output$PlotRadar <- renderPlot({
+    height = paste0("dBZ_",input$height)
+    date = as.Date(paste0(input$year, ".", input$date), format = "%Y.%m.%d")
+    radar_f <- radar %>% 
+      filter(Height == height) %>%
+      filter(Date == date)
     
-    #Create the data
-    
-    date <- input$DatesMerge
-    radar_f <- radar %>% filter(Date == date)
- 
-    # draw the histogram with the specified number of bins
-      tm_shape(UK) +
-        tm_fill(col = "lightgrey") +
-        tm_borders() +
-        tm_shape(radar_f) +
-        tm_dots(col = "N", palette = "viridis", style = "cont", legend.show = F) +
-        tm_layout(frame = FALSE)
-      
-      
+    if ((nrow(radar_f)) > 0) {
+    base_map +
+      tm_shape(radar_f) +
+      tm_dots(col = "N", palette = "viridis", style = "cont", legend.show = F)
+      } else {
+        base_map
+      }
   })
 })
+
+
 shinyApp(ui, server)
-
-
-
 
 
 
