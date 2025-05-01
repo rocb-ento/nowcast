bdc7535c0d8d3d99664334f60cd8bf658009d779
rocb-ento <bs21rocb@leeds.ac.uk>
2025-05-01 11:11:42 +0100

    tmap version with drop down menu for dates

--- nowcast_v0.R
+++ nowcast_v0.R
@@ -2,26 +2,75 @@ library(shiny)
 library(lubridate)
 library(data.table)
 library(dplyr)
+library(sf)
+library(rnaturalearthdata)
+library(rnaturalearth)
+library(tmap)
+
+# gridid_to_coords <- function(ID, n_cols = 12) {
+#   row <- ceiling(ID / n_cols)
+#   col <- ID - (row - 1) * n_cols
+#   return(list(row = row, col = col))
+# }
+# complete_grid <- data.frame(ID = as.numeric(1:144))  # 12x12 = 144 cells
+# complete_grid <- complete_grid %>%
+#   mutate(
+#     coords = lapply(ID, gridid_to_coords),
+#     row = sapply(coords, function(x) x$row),
+#     col = sapply(coords, function(x) x$col)
+#   ) %>%
+#   select(-coords)
+# radar <- left_join(complete_grid, radar, by = "ID")
+# available_dates <- sort(unique(radar$Date[!is.na(radar$Date)]))
+# mostobs <- radar %>% 
+#   group_by(ID, Radar, Date) %>% 
+#   unique() %>%
+#   ungroup() %>%
+#   group_by(Date) %>% 
+#   summarise(count = n()) %>% 
+#   arrange()
+
+### testdate
+date = as.Date("22/07/2021", format = "%d/%m/%Y")
+###
 
 radar <- fread("C:/Users/rocon/OneDrive/Documents/insect-radar-AES/data/radar_abundance_110325.csv") %>%
-  filter(radar == "Chenies") %>%
+  filter(!is.na(Latitude)) %>%
+  filter(Height == "dBZ_500") %>%
   mutate(Date = as.Date(Date)) %>%
-  mutate(ID = as.numeric(gsub("GridID_", "", ID)))
-gridid_to_coords <- function(ID, n_cols = 12) {
-  row <- ceiling(ID / n_cols)
-  col <- ID - (row - 1) * n_cols
-  return(list(row = row, col = col))
-}
-complete_grid <- data.frame(ID = as.numeric(1:144))  # 12x12 = 144 cells
-complete_grid <- complete_grid %>%
-  mutate(
-    coords = lapply(ID, gridid_to_coords),
-    row = sapply(coords, function(x) x$row),
-    col = sapply(coords, function(x) x$col)
-  ) %>%
-  select(-coords)
-radar <- left_join(complete_grid, radar, by = "ID")
-available_dates <- sort(unique(radar$Date[!is.na(radar$Date)]))
+  mutate(ID = as.numeric(gsub("GridID_", "", ID))) %>%
+  st_as_sf(., coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE) 
+
+radar_f <- radar %>% 
+  filter(Date == date)
+
+UK <- ne_countries(country = "united kingdom", type = "countries", scale = "medium")
+UK <- UK$geometry
+
+
+
+# Create the map with tmap
+tm_shape(UK) +
+  tm_fill(col = "lightgrey") +
+  tm_borders() +
+  tm_shape(radar_f) +
+  tm_dots(col = "N", palette = "viridis", style = "cont", legend.show = F) +
+  tm_layout(frame = FALSE)
+
+# ## leaflet version
+# 
+# library(leaflet)
+# 
+# leaflet(data = radar_f, options = leafletOptions(zoomControl = T, dragging = T)) %>%  #width = range(radar_f$Latitude), height = range(radar_f$Longitude))) %>%
+#   addPolygons(data = UK, fillColor = "grey", fillOpacity = 1, color = "white", weight = 1) %>%
+#   addCircleMarkers(
+#     ~Longitude, ~Latitude,
+#     color = ~colorNumeric("viridis", N)(N),  # Continuous color based on N
+#     radius = 4,
+#     popup = ~paste0("ID: ", ID, "<br>N: ", N)  # Popup displaying ID and N
+#   ) # %>%
+#   #setView(lng = mean(radar_f$Longitude), lat = mean(radar_f$Latitude), zoom = 5)
+
 
 ui <- shinyUI(fluidPage(
   
@@ -34,13 +83,18 @@ ui <- shinyUI(fluidPage(
       selectInput("DatesMerge",
                   "Select Date:",
                   choices = available_dates,
-                  selected = available_dates[length(available_dates) %/% 2]), # Select middle date as default
+                  selected = available_dates[length(available_dates) %/% 2]), 
+      
+      
     ),
+    
     mainPanel(
-      plotOutput("PlotRadar"))
+      # Increase the size of the plot by specifying height and width
+      plotOutput("PlotRadar", height = "700px", width = "1000px"))
     
   )
 ))
+
 
 
 server <- shinyServer(function(input, output) {
@@ -51,32 +105,19 @@ server <- shinyServer(function(input, output) {
     
     date <- input$DatesMerge
     radar_f <- radar %>% filter(Date == date)
-    if ( (nrow(radar_f) > 1) ){
+ 
     # draw the histogram with the specified number of bins
-      ggplot(radar_f, aes(x = col, y = row, fill = N)) +
-        geom_tile(color = "white", linewidth = 0.1) +  # Add thin white borders between cells
-        scale_fill_viridis_c(name = "Abundance", na.value = "grey90") +
-        scale_y_reverse() +  # Reverse Y-axis to have row 1 at the top
-        scale_x_continuous(breaks = 1:12, limits = c(1, 12)) +
-        scale_y_continuous(breaks = 1:12, limits = c(1, 12)) +
-        coord_equal() +  # Make cells square
-        theme_minimal() +
-        labs(title = "",
-             x = "grid X", 
-             y = "grid Y") +
-        theme(panel.grid = element_blank())
-      }
+      tm_shape(UK) +
+        tm_fill(col = "lightgrey") +
+        tm_borders() +
+        tm_shape(radar_f) +
+        tm_dots(col = "N", palette = "viridis", style = "cont", legend.show = F) +
+        tm_layout(frame = FALSE)
+      
+      
   })
 })
 shinyApp(ui, server)
-
-
-## TD
-
-
-
-
-
 
 
 
